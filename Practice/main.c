91b1b373a7da59c657ee38882564417f54e8d089
oh_jun <vhzkclq0705@naver.com>
2022-09-25 15:21:14 +0900

    Initial Commit

---
+++ Practice/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Practice
+//
+//  Created by 권오준 on 2022/09/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
